96b73bd81074a7a0251f401a82b5f3faef44d126
mikenye <mike.nye@xpanse.com.au>
2020-03-17 21:17:37 +0800

    Fix build script.

--- buildx.sh
+++ buildx.sh
@@ -4,6 +4,7 @@ IMAGE=mikenye/virtualradarserver
 
 # Build the image using buildx
 docker buildx build -t ${IMAGE}:latest --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
+docker pull ${IMAGE}:latest
 
 # Starting container to pull version from container logs
 VERSION=`docker run --rm --name get_vrs_version --entrypoint cat ${IMAGE}:latest /VERSION`
@@ -15,4 +16,3 @@ docker buildx build -t ${IMAGE}:${VERSION} --compress --push --platform linux/am
 
 # Kill the temporary container if its still running (timeout will kill it automatically after 5 seconds in case this script dies)
 docker kill get_vrs_version > /dev/null 2>&1
-
